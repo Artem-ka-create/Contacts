28db080528c9a498a4a7dfa765a16746909cf60a
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2022-01-18 01:03:30 +0100

    Delete Downloads/fff/spring_contacts_workflows directory

--- Downloads/fff/spring_contacts_workflows/src/main/java/com/spring/contacts/spring_contacts/models/ConfirmationTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.contacts.spring_contacts.models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, String> {
-	ConfirmationToken findByConfirmationToken(String confirmationToken);
-}
